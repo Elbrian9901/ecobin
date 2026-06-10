8a6c0da7e7f9eee14989f4d72e6470ad904cc635
Elbrian <elbriann199@gmail.com>
2026-06-10 23:05:27 +0700

    fix: compatibility PDO::MYSQL_ATTR_SSL_CA for PHP 8.5

--- config/database.php
+++ config/database.php
@@ -58,8 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            // SESUDAH - ini yang benar untuk PHP 8.5
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 
+                    ? Pdo\Mysql::ATTR_SSL_CA 
+                    : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
